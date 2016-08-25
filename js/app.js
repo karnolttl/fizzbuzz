19192dafef7dfe9adcd28d139e6dc4fc14cad5e6
karnolttl <tlkarnolt@gmail.com>
2016-08-24 21:34:14 -0400

    added input box functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
 $(document).ready (function() {
   var $ul = $('body > ul');
-  for(var i = 1; i <= 100; i++){
+
+  $('input').keyup(function (e) {
+    if (e.keyCode == 13) {
+      $('li').remove();
+      var num = ValidateNum($(this).val());
+      if (num)
+        fizzbuzz(num);
+      else
+        (function(){
+          $('p').append('<strong class="message" style="display:block; color:red">Invalid entry. Try again.</strong>');
+          setTimeout(function() {
+            $('.message').remove();
+          }, 4000);
+        })();
+    } // if the enterkey is pressed
+  });
+
+  function ValidateNum(val) {
+    console.log(val);
+    if(/[0-9]+/.test(val) && Number(val) > 1){
+      return Number(val);
+    }
+    return NaN;
+}
+
+
+  function fizzbuzz(len) {
+    for(var i = 1; i <= len; i++){
     var result = '';
     if((i%3) == 0)
       result = 'fizz';
@@ -9,5 +36,6 @@ $(document).ready (function() {
     if(!result)
       result = i;
     $ul.append("<li>"+result+"</li>");
+    }
   }
 });
